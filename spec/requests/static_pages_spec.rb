238faa10788753a6d3fcbccb19de320ced6c75a6
Aleks S <sav.dop@gmail.com>
2014-06-29 16:37:09 +0400

    Add full_title helper to rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,30 +8,28 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title("#{base_title} | Home") }
+   it { should have_content('Sample App') }
+   it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
     before { visit help_path }
-    
+
     it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-   before { visit about_path }
-    
     it { should have_content('About') }
-    it { should have_title("#{base_title} | About") }
-   end
+    it { should have_title(full_title('About Us')) }
+  end
 
   describe "Contact page" do
-
     before { visit contact_path }
-    
+
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
